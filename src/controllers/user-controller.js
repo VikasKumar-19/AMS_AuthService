025e9df032ff8e70c33a911be24225090ffc46a6
Vikas Kumar <vk.pratap98@gmail.com>
2022-12-30 13:46:41 +0530

    SignIn controller is added along with it's logic in service

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -25,6 +25,30 @@ const create = async (req, res) => {
   }
 };
 
+const signIn = async (req, res) => {
+  try {
+    const response = await userService.signIn(
+      req.body.email,
+      req.body.password
+    );
+    return res.status(200).json({
+      data: response,
+      success: true,
+      error: {},
+      message: "Successfully signed in",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      error: error,
+      message: "Something went wrong",
+    });
+  }
+};
+
 module.exports = {
-  create
-}
+  create,
+  signIn,
+};
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -29,6 +29,25 @@ class UserService {
     }
   }
 
+  async signIn(email, plainPassword) {
+    try {
+      //step 1-> fetch the user using the email
+      const user = await this.userRepository.getByEmail(email);
+      //step 2-> compare incoming plain password with stores encrypted password
+      const passwordsMatch = this.checkPassword(plainPassword, user.password);
+      if (!passwordsMatch) {
+        console.log("Password doesn't match");
+        throw { error: "Incorrect password" };
+      }
+      //step 3-> if password is match then create a token and send it to the FE.
+      const newJWT = this.createToken({ id: user.id, email: user.email });
+      return newJWT;
+    } catch (error) {
+      console.log("Something went wront in the sign in process");
+      throw error;
+    }
+  }
+
   createToken(user) {
     try {
       const result = jwt.sign(user, JWT_SECRET, { expiresIn: "1d" });
