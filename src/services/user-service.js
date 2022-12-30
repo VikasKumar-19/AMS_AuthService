177850c061f21bfa82a0bf67e103384bd0c84163
Vikas Kumar <vk.pratap98@gmail.com>
2022-12-30 13:05:58 +0530

    getUserById repo func and jwt create and verify logic added

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/server-config");
 const UserRepository = require("../repository/user-repository");
 
 class UserService {
@@ -21,6 +23,26 @@ class UserService {
       return response;
     } catch (error) {
       console.log("Something went wrong in service layer");
+      throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_SECRET, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation");
+      throw error;
+    }
+  }
+
+  verifyToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_SECRET);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation");
       throw error;
     }
   }
