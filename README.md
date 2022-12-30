080ae6dbd2e2554758c846bf672094bd88bcc8db
Vikas Kumar <vk.pratap98@gmail.com>
2022-12-30 18:35:04 +0530

    Added roles and user_roles through table

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Auth MicroService
+
+- Role Model -> to assign the roles to users. We will have many to many associations i.e user can have many roles and many users can belong to one role. We need to use third table throught which we can implement this.
+
+# TODO :-
+
+[*] send main with unique link which will verify the user.
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ const express = require("express");
 const { PORT } = require("./config/server-config");
 const apiRoutes = require("./routes");
 const app = express();
+const db = require("./models");
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use("/api", apiRoutes);
   app.listen(PORT, () => {
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
     console.log(`Server is running at http://localhost:${PORT}`);
   });
 };
